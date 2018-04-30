e10a410759c8507cecf4f856922b5074e99e4a7a
osedware <jmdiaz@howdeniberia.com>
2018-04-30 17:44:17 +0200

    Tema3

--- src/Controller/DeportesController.php
+++ src/Controller/DeportesController.php
@@ -8,10 +8,101 @@
 
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DeportesController
 {
-    public function inicio() {
-        return new Response("Mi primera pagina en Symfony!");
+    /**
+     * @param $_idioma
+     * @param $fecha
+     * @param $seccion
+     * @param $equipo
+     * @param $pagina
+     * @return Response
+     * @Route("/deportes/{_idioma}/{fecha}/{seccion}/{equipo}/{pagina}",
+     *     defaults={"slug":"1","formato":"html","pagina":"1"},
+     *     requirements={
+     *          "_idioma":"es|en",
+     *          "_formato":"html|json|xml",
+     *          "fecha":"[\d+]{8}",
+     *          "pagina"="\d+"
+     *     })
+     */
+    public function rutaAvanzadaListado($_idioma,$fecha,$seccion,$equipo,$pagina) {
+        return new Response(sprintf(
+            "Listado de noticias en idioma=%s,fecha=%s,deporte=%s,equipo=%s,pagina=%s",
+            $_idioma, $fecha, $seccion, $equipo, $pagina
+        ));
     }
+
+    /**
+     * @param $_idioma
+     * @param $fecha
+     * @param $seccion
+     * @param $equipo
+     * @param $slug
+     * @return Response
+     * @Route("/deportes/{_idioma}/{fecha}/{seccion}/{equipo}/{slug}.{_formato}",
+     *     defaults={"slug":"1","_formato":"html"},
+     *     requirements={
+     *          "_idioma":"es|en",
+     *          "_formato":"html|json|xml",
+     *          "fecha":"[\d+]{8}"
+     *     })
+     */
+    public function rutaAvanzada($_idioma,$fecha,$seccion,$equipo,$slug) {
+        return new Response(sprintf(
+            "Mi noticia en idioma=%s, fecha=%s, deporte=%s, equipo=%s, noticia=%s",
+            $_idioma, $fecha, $seccion, $equipo, $slug
+        ));
+    }
+
+    /**
+     * @param $seccion
+     * @param $pagina
+     * @return Response
+     * @Route("/deportes/{seccion}/{pagina}", name="lista_paginas",
+     *     requirements={"pagina"="\d+"},
+     *     defaults={"seccion":"tenis"})
+     */
+    public function lista($seccion,$pagina=1) {
+        return new Response(sprintf(
+            "Deportes seccion: seccion %s, listado de notcias página %s",
+            $seccion, $pagina));
+    }
+
+    /**
+     * @param $slug
+     * @param $seccion
+     * @return Response
+     * @Route("/deportes/{seccion}/{slug}",
+     *     defaults={"seccion":"tenis"})
+     */
+    public function noticia($slug, $seccion) {
+        return new Response(sprintf(
+            "Noticia de %s, con url dinámica=%s",
+            $seccion, $slug
+        ));
+    }
+
+    /**
+     * @return Response
+     * @Route("/deportes")
+     */
+    public function inicio() {
+        return new Response("Mi página de deportes!");
+    }
+
+    /**
+     * @param $slug
+     * @return Response
+     * @Route("/deportes/{slug}")
+     */
+    public function mostrar($slug) {
+        return new Response(sprintf(
+            "Mi articulo en mi pagina de deportes: Ruta %s",
+            $slug));
+    }
+
+
 }
